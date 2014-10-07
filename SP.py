1773906df9be484d1500deed2db78b123d1a5cb4
minddrummer <kesang0156357@gmail.com>
2014-10-07 18:01:52 -0400

    highlight the current available card value

--- SP.py
+++ SP.py
@@ -1,6 +1,6 @@
 __version__ = 'classic_secretary_problem_1.0, with pygame_version_1.9.2a0_fromUCI'
 
-# change button shape
+# 
 # highlight the current value that can be exploited
 # show turns used and left information
 # and also show all other information subjects need to know on the surface
@@ -29,12 +29,17 @@ class Rect:
     '''
     Rect is a class showing the card values
     '''
+    #class attributes
+    Click_Times = -1
     def __init__(self, clicker_counter, value, surface):
         self.width_pos, self.height_pos = self.compute_width_heigh_pos(clicker_counter)
         self.card_value = value
         self.cardwidth = CARDWIDTH
         self.cardheight = CARDHEIHT
         self.surface = surface
+        self.click_time = clicker_counter
+        #above is an instance attribute
+        Rect.Click_Times += 1
         
     def compute_width_heigh_pos(self, click_counter):
         '''
@@ -51,13 +56,21 @@ class Rect:
         return rect_width_pos,rect_height_pos
         
         
+    def regular_or_highlight(self):
+        #print 'Rect.Click_Times', Rect.Click_Times
+        #print 'self.click_time', self.click_time
+        if self.click_time == Rect.Click_Times:
+            pygame.draw.rect(self.surface, (255,255,255), (self.width_pos,self.height_pos,CARDWIDTH,CARDHEIHT), 0)
+        else:
+            pygame.draw.rect(self.surface, (150,150,150), (self.width_pos,self.height_pos,CARDWIDTH,CARDHEIHT), 0)
+            
     def draw(self):
         '''
         draw the rect instance and update its corresponding value
         '''
         color = (0, 139, 0)
         Rect = (self.width_pos, self.height_pos, self.cardwidth, self.cardheight)
-        pygame.draw.rect(self.surface, (255,255,255), (self.width_pos,self.height_pos,CARDWIDTH,CARDHEIHT), 0)
+        self.regular_or_highlight()
         #the following code is drawing a frame of the card
         pygame.draw.rect(self.surface, (6,113,148), (self.width_pos + 3,self.height_pos + 3, CARDWIDTH-6, CARDHEIHT-6), 3)
         self.draw_card_value()
@@ -68,11 +81,11 @@ class Rect:
         '''
         draw the card value on the Rect instance for each click
         '''
-        self.card_value
+        #self.card_value
         #first creat a Font instace
         font_instance = pygame.font.SysFont("comicsansms",20)
         #text1 is a new surface
-        text1=font_instance.render(str(self.card_value),True,(120,120,120))     
+        text1=font_instance.render(str(self.card_value),True,(77,77,77))     
         #render the new surface on the game interface
         self.surface.blit(text1, (self.width_pos + self.cardwidth/5, self.height_pos+ self.cardheight/5))
         #self.surface.blit(text1, (self.width_pos, self.height_pos))
@@ -99,7 +112,7 @@ pygame.display.flip()
 
 running = True
 click_counter = 0
-
+rect_set = []
 while running:
     for event in pygame.event.get():
         # buttonObj.handleEvent(event) is a queue
@@ -109,8 +122,16 @@ while running:
             #height_pos =
             card_value_generated = random.randint(LOWEST, HIGHEST)
             #print random.randint(LOWEST, HIGHEST)
+            
             rect = Rect(click_counter, card_value_generated, Window0)
+            if len(rect_set) == 0:
+                pass #pass here, execute nothing, but the control flow continue to the following statements
+            else:
+                rect_set[-1].draw()
             rect.draw()
+            rect_set.append(rect)
+            #print rect_set
+            
             
             click_counter += 1
         if event.type == pygame.QUIT:
